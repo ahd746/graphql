3bd36822fd6a30db0d3d6c1ee09c372a14da7447
Ahmed Salama <eng.ahd746@gmail.com>
2020-05-09 18:25:07 -0400

    V 1.0

---
+++ client/build/precache-manifest.2de7e85d9ac0a89631b2ea1d480ed1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebc9df9d7098fe5327b79d83552c12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d96fc199cd65b82d6b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a831289b6cdb1c5232ca",
+    "url": "/static/js/2.0a161df9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.0a161df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d96fc199cd65b82d6b",
+    "url": "/static/js/main.6f53ffef.chunk.js"
+  },
+  {
+    "revision": "38d122660160e028dd32",
+    "url": "/static/js/runtime-main.23ae2981.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema')
+const cors = require('cors')
+const path = require('path');
+
+
+
+
+const app = express();
+app.use(cors())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: false,
+  }),
+);
+//Middlewares
+app.use(express.static('./client/build'))
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+
+//Set the port and listen 
+PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
