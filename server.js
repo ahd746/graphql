12a456bec857c82c290a7be0c7d233875fcbe297
Ahmed Salama <eng.ahd746@gmail.com>
2020-05-10 06:40:10 -0400

    V 1.0

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use(express.static('./client/build'))
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
 //Set the port and listen 
-PORT = process.env.PORT || 6000;
+PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
